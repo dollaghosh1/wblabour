386002c94882b738ac63e4fa26c61274222e7325
dollaghosh1 <ghoshdola7@gmail.com>
2024-11-25 12:21:25 +0530

    eeror api error code

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,34 +1,26 @@
-// import mongoose from "mongoose";
-// import { DB_NAME } from "../constants.js";
 
-
-// const connectDB = async () => {
-//     try {
-//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-//     } catch (error) {
-//         console.log("MONGODB connection FAILED ", error);
-//         process.exit(1)
-//     }
-// }
-
-// export default connectDB
-
-//const { Client } = require('pg')
 import pg from 'pg'
+import { DB_NAME } from "../constants.js";
 const { Client } = pg
-const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'wblabour',
-  password: 'postgres',
-  port: 5432,
-})
-client.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-client.query(`select * from users`,(err,res)=>{
-  return console.log(res.rows)
-});
-export default client;
+const connectDB = async () => {
+    const client = new Client({
+      database: `${ DB_NAME }`,
+      user: 'postgres',
+      password: 'postgres',
+      host: 'localhost',
+      port: 5432,
+    })
+    try{
+      const connectionInstance = await client.connect(function(err) {
+        if (err) throw err;
+        console.log("DB Connected!");
+      });
+    }
+    catch (error){
+      console.log("DB connection FAILED ", error);
+      process.exit(1)
+      
+
+  }
+}
+export default connectDB;
